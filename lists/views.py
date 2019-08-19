bd207e9fd132e3232638d2a5c5bfcdf841e9ed53
Sreekaanth <sreekaanthgnsn3@gmail.com>
2019-08-19 16:31:46 -0400

    pg 68 TDD Goat

--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+# from django.http import HttpResponse
+from lists.models import Item
 # Create your views here.
 def home_page(request):
-    # if request.method == 'POST':
+    if request.method == 'POST':
+        # Item.objects.create(text=request.POST['item_text'])
+        Item.objects.create(text=request.POST['item_text'])
         # return HttpResponse(request.POST['item_text'])
-    return render(request,'home.html',{
-'new_item_text': request.POST.get('item_text', ''),})
+        return redirect('/')
+    return render(request,'home.html')
     # return render(request,'home.html',{'new_item_text': request.POST['item_text'],})
